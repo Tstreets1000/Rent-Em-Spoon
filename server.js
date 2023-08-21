f8747b688aaf758da12a7b54aac7a8493ee424f3
Tstreets1000 <terrance.jdhill@yahoo.com>
2023-08-21 17:24:55 -0400

    Final Code

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ require('./config/database')
 
 const app = require('./app-server')
 const mongoose = require('mongoose')
-const PORT = process.env.PORT || 8081
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
     console.log(`Shoot for the stars and land on the moon ${PORT} miles away`)
